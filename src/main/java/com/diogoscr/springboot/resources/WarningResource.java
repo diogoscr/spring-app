955e430710c1139b7ee7088b5b8fdb547bb01d51
diogoscr <diogoscr@gmail.com>
2021-02-11 14:17:02 -0300

    Warning delete

--- src/main/java/com/diogoscr/springboot/resources/WarningResource.java
+++ src/main/java/com/diogoscr/springboot/resources/WarningResource.java
@@ -2,6 +2,7 @@ package com.diogoscr.springboot.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,5 +44,11 @@ public class WarningResource {
         return ResponseEntity.created(uri).body(obj);
 
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    } 
 
 }
