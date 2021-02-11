f88a8fa9e0728b41d2120f96dd8aff001704d357
diogoscr <diogoscr@gmail.com>
2021-02-11 19:46:45 -0300

    Dev profile

--- src/main/java/com/diogoscr/springboot/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/diogoscr/springboot/services/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.diogoscr.springboot.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-
+    
     private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(Object id) {
